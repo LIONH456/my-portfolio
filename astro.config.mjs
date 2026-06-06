12211d48ef4862a1d350111c05deeb5e706a6046
lionh456 <junhongho25@gmail.com>
2026-06-07 00:24:28 +0700

    chore: install @astrojs/cloudflare and wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare'; // 1. 手动导入 Cloudflare 适配器
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'hybrid', // 2. 关键：设置为 hybrid（混合渲染）模式。这样全站默认是快如闪电的静态，唯独你的发信 API 跑在边缘服务器上。
+  adapter: cloudflare({
+    imageService: 'passthrough' // 3. 避免 Cloudflare 编译图片时报错，使用透传模式
+  }),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh', 'vi'],
