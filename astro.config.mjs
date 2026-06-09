c62501f74c63da9c73b0e73bf165d653ddbdf96b
lionh456 <junhongho25@gmail.com>
2026-06-09 12:26:28 +0700

    chore: align adapter mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'static', // 2. 关键：设置为 hybrid（混合渲染）模式。这样全站默认是快如闪电的静态，唯独你的发信 API 跑在边缘服务器上。
   adapter: cloudflare({
-    imageService: 'passthrough' // 3. 避免 Cloudflare 编译图片时报错，使用透传模式
+    imageService: 'passthrough'， // 3. 避免 Cloudflare 编译图片时报错，使用透传模式
+    mode: 'directory' 
   }),
   i18n: {
     defaultLocale: 'en',
